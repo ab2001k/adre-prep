e98561ac7ad815ab698acc1b868584fbdac197d2
Abhik Dutta <abhikdutta639@gmail.com>
2026-01-17 15:51:07 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,15 @@
 // sw.js
 self.addEventListener('install', (e) => {
   console.log('[Service Worker] Install');
+  self.skipWaiting(); // Forces this SW to activate immediately
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim()); // Takes control of the page immediately
 });
 
 self.addEventListener('fetch', (e) => {
-  // Basic fetch to satisfy PWA requirements
+  // Basic pass-through (Network Only strategy)
+  // This ensures users always get the latest version of your code
   e.respondWith(fetch(e.request));
 });
